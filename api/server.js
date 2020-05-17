bc6dafa1810c41fdb4dd8d1e1f36e451c84638ac
bbachi <bhargavbachina@yahoo.com>
2020-05-17 12:26:20 -0500

    initial commit

---
+++ api/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express(),
+      bodyParser = require("body-parser");
+      port = 3080;
+
+const users = [];
+
+app.use(bodyParser.json());
+app.use(express.static(process.cwd()+"/my-app/dist/angular-nodejs-docker-compose/"));
+
+app.get('/api/users', (req, res) => {
+  console.log('dfsdfsd')
+  res.json(users);
+});
+
+app.post('/api/user', (req, res) => {
+  const user = req.body.user;
+  users.push(user);
+  res.json("user addedd");
+});
+
+app.get('/', (req,res) => {
+  res.sendFile(process.cwd()+"/app-ui/dist/angular-nodejs-docker-compose/index.html")
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on the port::${port}`);
+});
